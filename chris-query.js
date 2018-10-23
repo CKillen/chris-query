e6c8dd3aed4f29a3f8f0e6b964d771f3eabc5cfc
CKillen <cl_killen@hotmail.com>
2018-10-23 19:58:57 -0400

    class, empty html functions addded
    Added the ability to add and remove classes, also added the
    ability to to empty the selected and to grab the html of the
    selected.

--- chris-query.js
+++ chris-query.js
@@ -1,5 +1,29 @@
 const cq = el => {
-  let element = document.querySelectorAll(el);
+  let element = null;
+
+  if(el.includes("<") && el.includes(">"))
+  {
+    //create a nodelist like elmenet for new div
+    element = Object.create(NodeList, {
+      '0': {value: document.createElement(el.substring(1, el.length -1)), enumerable: true},
+      'length': {value: 1},
+      'item': {
+          "value": function (i) {
+              return this[+i || 0];
+          }, 
+          enumerable: true
+      }
+    });
+    //element = Object.create(NodeList, {0: document.createElement(el.substring(1, el.length -1))});
+  }
+  else
+  {
+    element = document.querySelectorAll(el);
+    
+  }
+
+  let elementClass = "";
+  let elementHtmlList = [];
   let elementStyle = "";
   return {
     log: function() {
@@ -56,6 +80,7 @@ const cq = el => {
     {
       elementStyle += `${style}: ${value};`;
 
+      
       for(let i = 0; i < element.length; i++)
       {
         element[i].setAttribute("style", elementStyle);
@@ -71,34 +96,75 @@ const cq = el => {
       }
       return this;
     },
+    append: function(appendElement)
+    {
+
+      for(let i = 0; i < appendElement.getElements().length; i++)
+      {
+
+        element[element.length -1].appendChild(appendElement.getElements()[i]);
+      }
+
+      return this;
+    },
+    text: function(text)
+    {
+      
+      for(let i = 0; i < element.length; i++)
+      {
+        let textNode = document.createTextNode(text);
+        element[i].appendChild(textNode);
+      }
+      return this; 
+    },
+    addClass: function(addClass)
+    {
+      elementClass += " " + addClass;
+
+      for(let i = 0; i < element.length; i++)
+      {
+        element[i].className = elementClass;
+      }
+      return this;
+    },
+    removeClass: function(removeClass)
+    {
+      elementClass = elementClass.replace(removeClass, "");
+      for(let i = 0; i < element.length; i++)
+      {
+        element[i].className = elementClass;
+      }
+      return this;
+    },
+    empty: function()
+    {
+      for(let i = 0; i < element.length; i++)
+      {
+        element[i].innerHTML = "";
+      }
+      return this; 
+    },
+    html: function()
+    {
+      for(let i = 0; i < element.length; i++)
+      {
+        elementHtmlList.push(element[i].innerHTML);
+      }
+      console.log(elementHtmlList);
+    },
+    getElements: function()
+    {
+      let elementList = [];
+
+      for(let i = 0; i < element.length; i++)
+      {
+        elementList.push(element[i]);
+      } 
+
+      return elementList;
+    }
   }
 }
-
-const chris = {
-  element: null,
-  elementCall: null,
-  style: "",
-  grab: function(domElement)
-        {
-          element = document.querySelectorAll(domElement);
-
-          return this;
-        },
-  log: function()
-        {
-          console.log(element);
-        },
-
-
-
-
-    append: function(element)
-        {
-
-        }
-};
-
-cq("#change-").off("click")
 
 
 cq(".chris").addCss('height', '100px').addCss('background', 'red').addCss("border" , "12px black solid").removeCss("background");
@@ -116,4 +182,6 @@ cq("#change-off").on("click", function()
 {
   cq("#test").off("click")
 });
+
+cq(".chris").addClass("test").addClass("true");
 
